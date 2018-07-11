3ebed9068039761b3beb568e626245efc14dfc85
lchancri <lchancri@student.42.fr>
2018-07-11 18:43:04 +0200

    tentative histogram

--- histogram.py
+++ histogram.py
@@ -28,7 +28,10 @@ for i in range(0, col):
             a = a + 1
             
 print("\n\nnb_nan = ", a, "\n\n")
-print(X[:,15])
 #plt.hist(X[:,15])
-plt.hist(X[:,15], bins=1)
+age = [10, 25, 45, 65, 74 ,21 ,35 ,21 ,21 ,35]
+bins = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+#bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
+#plt.hist(age, bins, histtype='bar', rwidth=0.8)
+plt.hist(X[:,15], bins, histtype='bar', rwidth=0.8)
 plt.show()
