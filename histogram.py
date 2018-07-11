fd3abb95c507c2de3fcf85e5d5d6bb1f102d2470
lchancri <lchancri@student.42.fr>
2018-07-11 19:01:21 +0200

    tentative histogram

--- histogram.py
+++ histogram.py
@@ -12,14 +12,16 @@ X = [np.insert(row, 0, 1) for row in data.drop(["Hogwarts House"], axis=1).value
 test = [np.insert(row, 0, 1) for row in data.drop(["Hogwarts House"], axis=1).values]
 X = np.reshape(X, (line, col))
 #test = np.reshape(X, (line, col))
+
 q = 0
 for key in data:
     print(q, "  ", key)
     q = q + 1
-plt.xlabel('Result')
-plt.ylabel('Number of student')
+
+
 #nan = np.arange(line * col).reshape(line, col)
 #print(np.isnan(nan))
+
 a = 0
 for i in range(0, col):
     for j in range(0, line):
@@ -27,11 +29,17 @@ for i in range(0, col):
             X[j][i] = 0
             a = a + 1
             
+
+for i in range(0, line):
+    print(X[i][15])
+
 print("\n\nnb_nan = ", a, "\n\n")
-#plt.hist(X[:,15])
-age = [10, 25, 45, 65, 74 ,21 ,35 ,21 ,21 ,35]
-bins = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+plt.xlabel('Result')
+plt.ylabel('Number of student')
+age = [10, 25, 10.1, 10.2, 10.3, 10.4, 45, 65, 74 ,21 ,35 ,21 ,21 ,35, 28, 29]
 #bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
 #plt.hist(age, bins, histtype='bar', rwidth=0.8)
-plt.hist(X[:,15], bins, histtype='bar', rwidth=0.8)
+bins = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+plt.hist(X[:,15], bins, histtype='bar')
 plt.show()
