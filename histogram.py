61ba80619fe5d74c12e1e7d531d8e054ca5ca53f
lchancri <lchancri@student.42.fr>
2018-07-12 19:14:53 +0200

    tentative histogram

--- histogram.py
+++ histogram.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 #help(plt.hist)
-data = pd.read_csv("ressources/dataset_test.csv")
+data = pd.read_csv("ressources/dataset_train.csv")
+print(data)
 line, col = np.shape(data)
 print(line, col)
 Y = data["Hogwarts House"]
@@ -38,16 +39,47 @@ for i in range(0, line):
         _min = X[i][15]
     if (_max < X[i][15]):
         _max = X[i][15]
-#    print(X[i][15])
 
 print("\n\nnb_nan = ", a, "\n\n")
 
 print(_min, _max)
-plt.xlabel('Result')
-plt.ylabel('Number of student')
+#plt.xlabel('Result')
+#plt.ylabel('Number of student')
 age = [10, 25, 10.1, 10.2, 10.3, 10.4, 45, 65, 74 ,21 ,35 ,21 ,21 ,35, 28, 29]
 #bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
 #plt.hist(age, bins, histtype='bar', rwidth=0.8)
 bins = [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-plt.hist(X[:,15], bins, histtype='bar', cumulative=True)
+#plt.hist(X[:,15], bins, histtype='bar', cumulative=True)
+#for i in range(0, col):
+#    plt.hist(X[:,i], bins = 'auto')
+
+
+
+
+for j in range(0, col):
+    house_1 = []
+    house_2 = []
+    house_3 = []
+    house_4 = []
+    for i in range(0, line):
+        if (Y[i] == 'Hufflepuff'):
+            house_1.append(X[i, j])
+        if (Y[i] == 'Slytherin'):
+            house_2.append(X[i, j])
+        if (Y[i] == 'Gryffindor'):
+            house_3.append(X[i, j])
+        if (Y[i] == 'Ravenclaw'):
+            house_4.append(X[i, j])
+    plt.hist([house_1, house_2, house_3, house_4], bins = 'auto', color = ['yellow', 'red', 'green', 'black'], edgecolor = 'blue')
+    plt.show()
+y = []
+for i in range(0, line):
+    y.append(X[i, 16])
+#plt.hist(x, edgecolor = 'red', color = 'black')
+plt.hist([x, y], bins = 'auto', color = ['blue', 'red'])
+
+
+
+
+
 plt.show()
