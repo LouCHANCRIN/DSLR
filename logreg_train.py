7704992f8040b51eb87b488721541d1fd131a2dd
lchancri <lchancri@student.42.fr>
2018-07-24 23:10:49 +0200

    logreg

--- logreg_train.py
+++ logreg_train.py
@@ -9,8 +9,10 @@ Y= np.reshape(Y, (line, 1))
 X = [np.insert(row, 0, 1) for row in data.drop(["Hogwarts House", "First Name",
     "Last Name", "Birthday", "Index", "Best Hand", "Arithmancy",
     "Care of Magical Creatures"], axis=1).values]
+
 col -= 7
 X = np.reshape(X, (line, col))
+
 theta = [[0.0] * col]
 theta = np.reshape(theta, (col, 1))
 
@@ -60,22 +62,39 @@ def scale(X, line, col):
             X[l][c] = (X[l][c] - _mean[c]) / (_max[c] - _min[c])
     return (X)
 
+def hypothese(Xline, theta, l):
+    TX = Xline
+    p = TX.dot(theta)
+    ret = 1 / (1 + np.exp(-p))
+    return (ret)
 
+def cost(X, Y, theta, line, c, expected):
+    som = 0
+    for l in range(0, line):
+        if (Y[l] == expected):
+            y = 0
+        else:
+            y = 1
+        hyp = hypothese(X[l], theta, l)
+        res = (hyp - y) * X[l][c]
+        som += res
+    return (som)
 
-
-
-
-
-def log_reg(X, Y, theta, line, col, alpha, num_iters):
+def log_reg(X, Y, theta, line, col, alpha, num_iters, result):
     temp = [[0.0] * col]
     temp = np.reshape(temp, (col, 1))
     for i in range(0, num_iters):
-        for c in range(0, col):
-            temp[c] = ...
-        for c in range(0, col):
-            theta[c] = temp[c]
+        print(i)
+        for exp in range(0, 3):
+            for c in range(0, col):
+                temp[c] = theta[c] - (alpha * cost(X, Y, theta, line, c, result[exp]))
+            for c in range(0, col):
+                theta[c] = temp[c]
     return (theta)
 
+result = ['Hufflepuff', 'Gryffindor', 'Slytherin', 'Ravenclaw']
+alpha = 0.01
+num_iters = 1500
 X = change_nan(X, col, line)
 X = scale(X, line, col)
-print(X)
+theta = log_reg(X, Y, theta, line, col, alpha, num_iters, result)
